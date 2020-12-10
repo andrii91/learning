73d38c1c930ff53ad16e39027fde0a478a1bcb1d
Andrii <litvin.andriy91@gmail.com>
2020-12-10 21:45:46 +0200

    add pr

---
+++ dist/js/app.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(window).scroll(function(){return $(".nav").toggleClass("fixed",$(window).scrollTop()>0)}),$("#nav-icon").click(function(){$(this).toggleClass("open"),$(this).parents("nav").toggleClass("open"),$("#menu").slideToggle(200)})});

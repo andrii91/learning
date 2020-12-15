c265452def33d33847147603e54e9cd9b759c2ce
Andrii <litvin.andriy91@gmail.com>
2020-12-15 12:06:24 +0200

    fixnew

--- src/js/component.js
+++ src/js/component.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     $('#nav-icon').click(function () {
     $(this).toggleClass('open');
     $(this).parents('nav').toggleClass('open');
-    $('#menu').slideToggle(200);
+    $('#menu').slideToggle(0);
       $('header, main,footer').toggleClass('blur');
       $('body').toggleClass('overflow-hidden')
 

eefd8b1bc6b0708151e931dfdaad1313b6a58e69
Andrii <litvin.andriy91@gmail.com>
2020-12-16 17:09:24 +0200

    fix animation

--- src/js/component.js
+++ src/js/component.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
   });
   
     $('#nav-icon').click(function () {
+      $('.hamburger').toggleClass('is-active')
+    $('.nav').toggleClass('menu-open')
+      
     $(this).toggleClass('open');
     $(this).parents('nav').toggleClass('open');
     $('#menu').slideToggle(0);
@@ -12,4 +15,30 @@ $(document).ready(function() {
 
   });
 
+  
+  
+  
+  function hamburger() {
+/*    if ($('.hamburger').hasClass('is-active') && !$('modal').is(':visible')) {
+        $('body').removeClass('noScroll')
+    } else $('body').addClass('noScroll')
+    if (!$('.header').hasClass('header_fixed')) {
+        $('.main').height($('.main').height())
+        $('.nav').addClass('nav_fixed')
+    }
+    if ( $('.nav').hasClass('menu-open') && $(window).scrollTop() <= 27) {
+        $('.nav').removeClass('nav_fixed')
+    }*/
+//    $('.hamburger').toggleClass('is-active')
+//    $('.nav').toggleClass('menu-open')
+//    $('.content').toggleClass('blur')
+}
+
+      $('.hamburger').click(hamburger)
+
+    $('.header__item').click(function (e) {
+        if ($(window).width() <= 1023) hamburger()
+    })
+  
+  
 });
